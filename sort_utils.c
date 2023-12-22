5a04b67f09266323a9a4f63868e5af7aef66db61
Suhana Sajid <73544349+Suhana66@users.noreply.github.com>
2023-12-22 20:58:26 +0400

    completed function to do move

--- sort_utils.c
+++ sort_utils.c
@@ -6,7 +6,7 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 12:13:56 by susajid           #+#    #+#             */
-/*   Updated: 2023/12/22 17:34:03 by susajid          ###   ########.fr       */
+/*   Updated: 2023/12/22 20:57:37 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,24 @@ int	find_move(t_stack *stack, int len, int insert, bool if_desc)
 
 void	do_move(t_sorting *sorting, int a_move, int b_move)
 {
+	while (a_move < 0 && b_move < 0 && a_move++ && b_move++)
+	{
+		reverse_rotate(sorting, 'a', false);
+		reverse_rotate(sorting, 'b', false);
+		ft_printf("rrs\n");
+	}
+	while (a_move > 0 && b_move > 0 && a_move-- && b_move--)
+	{
+		rotate(sorting, 'a', false);
+		rotate(sorting, 'b', false);
+		ft_printf("rs\n");
+	}
+	while (a_move < 0 && a_move++)
+		reverse_rotate(sorting, 'a', false);
+	while (a_move > 0 && a_move--)
+		rotate(sorting, 'a', false);
+	while (b_move < 0 && b_move++)
+		reverse_rotate(sorting, 'b', false);
+	while (b_move > 0 && b_move--)
+		rotate(sorting, 'b', false);
 }
